a9e4624b8dc6bd704d00663d89343ef9d497375b
cyx <774169864@qq.com>
2020-02-29 21:51:30 +0800

    添加注释

--- src/main/java/com/cyx/residential_estate_property_management/Vo/MenuVo.java
+++ src/main/java/com/cyx/residential_estate_property_management/Vo/MenuVo.java
@@ -1,52 +1,18 @@
 package com.cyx.residential_estate_property_management.Vo;
 
+import lombok.Data;
+
 import java.util.List;
 
 /**
+ * 菜单的Vo
  * @author cyx
  * @data 2020/1/7 14:29
  */
+@Data
 public class MenuVo {
     private int psid;
     private String name;
     private String path;
     private List<MenuVo> children;
-
-    public List<MenuVo> getChildren() {
-        return children;
-    }
-
-    public void setChildren(List<MenuVo> children) {
-        this.children = children;
-    }
-
-    public int getPsid() {
-        return psid;
-    }
-
-    public void setPsid(int psid) {
-        this.psid = psid;
-    }
-
-    public MenuVo(int psid, String name, String path) {
-        this.psid = psid;
-        this.name = name;
-        this.path = path;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPath() {
-        return path;
-    }
-
-    public void setPath(String path) {
-        this.path = path;
-    }
 }
